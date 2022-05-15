7a160ab97f900e10b4cb16bd2fa6ac107a8f0d63
Piwko <marcinpiwko7@gmail.com>
2022-05-15 21:14:11 +0200

    Add project

---
+++ app/functional_tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+from selenium import webdriver
+
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
